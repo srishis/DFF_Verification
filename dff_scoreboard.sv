9251c9d6d9e406b33b98b1cdbd9bdea03db245f3
Rishitosh Sawant <srishis.214@gmail.com>
2020-03-28 18:28:33 -0700

    Update dff_scoreboard.sv

--- dff_scoreboard.sv
+++ dff_scoreboard.sv
@@ -1,8 +1,8 @@
-// In progress
+
 class dff_sb;
   
   //used to count the number of transactions
-  int num_trans;
+  static int num_trans;
   
   dff_transaction  tx, tx1;
   
@@ -11,17 +11,18 @@ class dff_sb;
     forever begin
       dff_cfg::mon2sb.get(tx);
       dff_cfg::ref2sb.get(tx1);
-      if(tx.d == 0)
-        if((tx.q !== tx1.q) && (tx.qbar !== tx1.qbar))
-          $display("ERROR:DFF_SCOREBOARD: Mismatch found :: Expected Data = %0p, Actual value = %0p", tx, tx1);
-      else if(tx.d == 1)
-        if((tx.q !== tx1.q) && (tx.qbar !== tx1.qbar))
-          $display("ERROR:DFF_SCOREBOARD: Mismatch found :: Expected Data = %0h, Actual value = %0h");
-        
-      
+      if((tx.d == 0) && (tx1.d == 0))
+        if((tx.q !== tx1.q) && (tx.qbar !== tx1.qbar)) begin
+          $display("ERROR:DFF_SCOREBOARD: Mismatch found when d is equal to 0 :: Expected Data = %0p, Actual value = %0p", tx, tx1);
+          dff_cfg::error_count++;
+        end
+      else if((tx.d == 1) && (tx1.d == 1))
+        if((tx.q !== tx1.q) && (tx.qbar !== tx1.qbar)) begin
+          $display("ERROR:DFF_SCOREBOARD: Mismatch found when d is equal to 1 :: Expected Data = %0p, Actual value = %0p");
+          dff_cfg::error_count++;
+        end
+      num_trans++;
     end
-    
-    
-    
+ 
   endtask
 endclass
