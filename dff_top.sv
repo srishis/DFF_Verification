812351dea1e5cc911a161e0f09afe3106b82385a
Rishitosh Sawant <srishis.214@gmail.com>
2020-03-28 17:52:44 -0700

    Update dff_top.sv

--- dff_top.sv
+++ dff_top.sv
@@ -1,16 +1,20 @@
 module dff_top;
-
+	
+	parameter CLK_PERIOD 	 = 5;
+	parameter GLOBAL_TIMEOUT = 1000;
+	
 	// top level clock and reset signals
 	bit clk, rst;
 
 	//generate top level clock to be passed to the design and test bench
 	initial begin
-		clk = 0;					// not required as declared bit, why?
-		forever #5 clk = ~clk;	// assuming 100Mhz clock frequency hence #5ns, calculate the time period based on system clock requirements
+		clk = 0;				// not required as declared bit, why?
+		forever #CLK_PERIOD clk = ~clk;		// assuming 100Mhz clock frequency hence #5ns, calculate the time period based on system clock requirements
 	end
 
 // Lift global reset
  initial begin
+   repeat(1) @(posedge clk);
    rst = 1;
    // wait few clocks cycles for system to stabilize before lifting reset, varies from design to design based on the complexity 
    //it might be even as high as 1000 clock cycles for an complex SoC, refer architectural specifications for getting the number
@@ -18,18 +22,24 @@ module dff_top;
    rst = 0;
  end
  
-	// DFF main interface instantiation
+	// DFF main physical interface instantiation
 	dff_if pif(clk, rst);
 
 	// DFF design instantiation
-	dff dut(pif);
+	dff DUT (pif);
 
 	// DFF Testbench instantiation
-	dff_tb tb;
+	dff_tb TB ();
 
 	// hook up design and Testbench using physical interface handle pif to virtual interface handle
 	initial begin
 	dff_cfg::vif = pif;
 	end
+	
+	 // Test case ending using finish
+	 initial begin
+  	 	#GLOBAL_TIMEOUT;
+  	 	$finish;
+ 	end
 
 endmodule
