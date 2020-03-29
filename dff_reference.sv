20bb854302e36627bf79f4616f1a57d0cf3c8c96
Rishitosh Sawant <srishis.214@gmail.com>
2020-03-28 19:25:17 -0700

    Update dff_reference.sv

--- dff_reference.sv
+++ dff_reference.sv
@@ -5,11 +5,13 @@ class dff_ref_model;
   task run();
     forever begin
       dff_cfg::gen2ref.get(tx);
-      //tx_copy = new;
       tx_copy.copy(tx);
+      
       if(tx_copy.d == 0) begin tx_copy.q = 0; tx_copy.qbar = 1; end
       else               begin tx_copy.q = 1; tx_copy.qbar = 0; end
+      
       dff_cfg::ref2sb.put(tx_copy);
     end  
   endtask
+  
 endclass
