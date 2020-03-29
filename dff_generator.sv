79f002b8872cf25c9783050f4b4cc5ac4b7f603c
Rishitosh Sawant <srishis.214@gmail.com>
2020-03-28 21:13:06 -0700

    Update dff_generator.sv

--- dff_generator.sv
+++ dff_generator.sv
@@ -10,8 +10,7 @@ class dff_generator;
 	repeat(10) begin
 	tx = new();
 	if(!tx.randomize()) $display("ERROR:DFF GENERATOR::RANDOMIZATION FAILED");
-	//txQ.push_back(tx);
-		
+	//txQ.push_back(tx;	
 	// mailbox for inter communication between classes
 	dff_cfg::gen2drv.put(tx); 	// send transaction to Driver to drive it to the DUT using mailbox put() method
 	dff_cfg::gen2ref.put(tx); 	// send transaction to reference model for generating expected values using mailbox put() method
