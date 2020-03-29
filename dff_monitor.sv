53106ff010a7593d9503942887ac537c6c22e7b7
Rishitosh Sawant <srishis.214@gmail.com>
2020-03-28 19:49:19 -0700

    Update dff_monitor.sv

--- dff_monitor.sv
+++ dff_monitor.sv
@@ -3,7 +3,7 @@ class dff_monitor;
 	// transaction class handle (not object!) to use in this class
 	dff_transaction tx;
 	// As monitor interacts with the interface it needs a virtual interface handle!
-	virtual	dff_if.MON_TB vif;
+	virtual	dff_if.MON vif;
 
 	function new();
 		this.vif = dff_cfg::vif;
@@ -12,7 +12,7 @@ class dff_monitor;
 	task run();
 		forever begin
 		// always create a new transaction object for each transaction to avoid conflicts
-		tx 	 	= new;		// transaction class object created
+		tx 	= new;		// transaction class object created
 		tx.d 	= vif.d;
 		tx.q 	= vif.q;
 		tx.qbar = vif.qbar;
