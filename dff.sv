d3a332dee0f92fd3f711f24e19cb7969e41fc2b4
Rishitosh Sawant <srishis.214@gmail.com>
2020-03-28 17:39:03 -0700

    Update dff.sv

--- dff.sv
+++ dff.sv
@@ -3,9 +3,9 @@ module dff(dff_if.DUT dif);
 	assign dif.qbar = ~dif.q;
 	
 	always_ff@(posedge dif.clk) begin
-		if(dif.rst)	dif.q <= 0;
+		if(dif.rst)	dif.q <= '0;
 		else begin
-			assert(!$isunknown(d));		// design assertion to catch if D input provided to the design is unknown
+			assert(!$isunknown(dif.d));		// design assertion to catch if D input provided to the design is unknown
 			dif.q <= dif.d;
 		end	
 	end
