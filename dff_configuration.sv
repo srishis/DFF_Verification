7967a15e225afb9c27eca8036ad3907a2b6288ed
Rishitosh Sawant <srishis.214@gmail.com>
2020-03-28 18:10:28 -0700

    added mailbox for reference model

--- dff_configuration.sv
+++ dff_configuration.sv
@@ -2,8 +2,10 @@ class dff_cfg;
   
   // create all mailboxes required in the environment 
   static mailbox gen2drv;
+  static mailbox gen2ref;
   static mailbox mon2cov;
   static mailbox mon2sb;
+  static mailbox ref2sb;
   
   // create virtual interface handle to pass to all classes from top
   static virtual dff_if vif;
