d23dca8ca9c1329e02d06181f27cc3e91c02b02b
Rishitosh Sawant <srishis.214@gmail.com>
2020-03-28 18:12:36 -0700

    Update dff_generator.sv

--- dff_generator.sv
+++ dff_generator.sv
@@ -8,10 +8,12 @@ task run();
 	// simple test for DFF with 10 random transactions
 	repeat(10) begin
 	tx = new();
-	if(!tx.randomize())$display("DFF GENERATOR::RANDOMIZATION FAILED");
-	txQ.push_back(tx);
+	if(!tx.randomize()) $display("ERROR:DFF GENERATOR::RANDOMIZATION FAILED");
+	//txQ.push_back(tx);
+		
 	// mailbox for inter communication between classes
 	dff_cfg::gen2drv.put(tx); 	// send transaction to Driver to drive it to the DUT using mailbox put() method
+	dff_cfg::gen2ref.put(tx); 	// send transaction to reference model for generating expected values using mailbox put() method
 end
 endtask
 
